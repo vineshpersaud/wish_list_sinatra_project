d01f59f1c0cea74139a618e1b166af608acdf5d8
codeesh <vineshpersaud@gmail.com>
2018-05-21 19:42:55 -0400

    add validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ActiveRecord::Base
+  validates_presence_of :name
   belongs_to :wishlists
 end
--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,4 +1,5 @@
 class WishList < ActiveRecord::Base
+  validates_presence_of :name
   belongs_to :wish_list
   has_many :items
 end
