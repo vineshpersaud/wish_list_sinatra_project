47147645cc8254aac19ab2d100b699820a1afdc6
codeesh <vineshpersaud@gmail.com>
2018-05-21 16:55:34 -0400

    add more fields to items

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -2,7 +2,7 @@ class ItemController < ApplicationController
 
   post '/item/new' do
     @wishlist = WishList.find_by_id(params[:wishlist_id])
-    item = @wishlist.items.build(:name => params[:item])
+    item = @wishlist.items.build(:name => params[:item],:quantity => params[:quantity],:price => params[:price])
     item.save
     redirect "wishlist/show/#{@wishlist.id}" 
   end
