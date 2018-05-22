74a2845ccaa6061ce27e57afcb5cffee1c2168f7
codeesh <vineshpersaud@gmail.com>
2018-05-21 22:40:37 -0400

    add security can no longer see delete buttons if this is not users wishlist

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -3,25 +3,28 @@ class ItemController < ApplicationController
   post '/item/new' do
     @wishlist = WishList.find_by_id(params[:wishlist_id])
     if logged_in?
-      if current_user.id == @wishlist.id && @wishlist
+      if @wishlist && current_user.id == @wishlist.user_id 
         item = @wishlist.items.build(:name => params[:item],:quantity => params[:quantity],:price => params[:price])
         item.save
-      end
-      if item.errors.include?(:name)
-        flash[:alert] = "Item must have name."
-      end
-    else
+        if item.errors.include?(:name)
+          flash[:alert] = "Item must have name."
+        end
       redirect "wishlist/show/#{@wishlist.id}" 
+      end  
     end
-
+    redirect "wishlist/show/#{@wishlist.id}" 
   end
 
   post '/item/:id/delete' do #delete action
     @item = Item.find_by_id(params[:id])
     @wishlist = @item.wish_list_id
-    @item.delete
-    flash[:message] = "Item successfully deleted."
-    redirect "/wishlist/show/#{@wishlist}"
+    if logged_in?
+      if current_user.id == WishList.find_by_id(@wishlist).user_id
+        @item.delete
+        flash[:message] = "Item successfully deleted."
+        redirect "/wishlist/show/#{@wishlist}"
+      end
+    end
   end
 
 end
