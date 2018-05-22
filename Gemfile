8fe8c532c148c4d8c4427984e2d5128d58bc130f
codeesh <vineshpersaud@gmail.com>
2018-05-22 14:53:31 -0400

    change enviroment

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'require_all'
 gem 'bcrypt'
 gem 'rack-flash3'
 gem 'thin'
+gem 'dotenv'
 
 group :development do
  gem 'sqlite3'
--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,7 @@
 ENV['SINATRA_ENV'] ||= "development"
+require 'bundler/setup'
+require 'dotenv'
+Dotenv.load
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
